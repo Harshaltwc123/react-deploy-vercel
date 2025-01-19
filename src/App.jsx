06c75c43031cfc93f374ecb54c2065fee59dd70b
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 15:17:30 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./component/Navbar";
-import Home from "./pages/Home";
-import WhatWeOffer from "./pages/WhatWeOffer";
-import Events from "./pages/Events";
-import HubnexLabs from "./pages/HubnexLabs";
-import GetInTouch from "./pages/GetInTouch";
-import AboutUs from "./pages/AboutUs";
-import ApplyNow from "./pages/ApplyNow";
-import Footer from "./component/Footer";
+
 
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/what-we-offer" element={<WhatWeOffer />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/hubnexlabs" element={<HubnexLabs />} />
-        <Route path="/get-in-touch" element={<GetInTouch />} />
-        <Route path="/about-us" element={<AboutUs />} />
-        <Route path="/apply-now" element={<ApplyNow />} />
-      </Routes>
-      <Footer />
-    </Router>
+   <div>
+  <h1 className="bg-red-700">hello</h1>
+   </div>
  
   );
 }
