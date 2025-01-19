7a282582a96fd84edfb08781f24b4b73fe6aa5bf
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 16:03:55 +0530

    sisteenth commit

---
+++ src/component/Scrollingcard.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import hubnex1 from "../assets/hubnex1.png";
+import hubnex2 from "../assets/hubnex2.png";
+import hubnex3 from "../assets/hubnex3.png";
+import hubnex4 from "../assets/hubnex4.png";
+import hubnex5 from "../assets/hubnex5.png";
+import hubnex6 from "../assets/hubnex6.png";
+import hubnex7 from "../assets/hubnex7.png";
+import hubnex10 from "../assets/hubnex10.png";
+import hubnex11 from "../assets/hubnex11.png";
+import hubnex12 from "../assets/hubnex12.png";
+import hubnex13 from "../assets/hubnex13.png";
+import hubnex14 from "../assets/hubnex14.png";
+import hubnex15 from "../assets/hubnex15.png";
+import hubnex16 from "../assets/hubnex16.png";
+import hubnex17 from "../assets/hubnex17.png";
+import hubnex18 from "../assets/hubnex18.png";
+import hubnex19 from "../assets/hubnex19.png";
+import hubnex20 from "../assets/hubnex20.png";
+import hubnex21 from "../assets/hubnex21.png";
+import hubnex22 from "../assets/hubnex22.png";
+import hubnex23 from "../assets/hubnex23.png";
+import hubnex24 from "../assets/hubnex24.png";
+import hubnex25 from "../assets/hubnex25.png";
+import hubnex26 from "../assets/hubnex26.png";
+import hubnex27 from "../assets/hubnex27.png";
+import hubnex28 from "../assets/hubnex28.png";
+import hubnex29 from "../assets/hubnex29.png";
+import hubnex30 from "../assets/hubnex30.png";
+import hubnex31 from "../assets/hubnex31.png";
+import hubnex32 from "../assets/hubnex32.png";
+import hubnex33 from "../assets/hubnex33.png";
+
+function Scrollingcard() {
+  return (
+    <div className="relative mt-16">
+      <div className="grid grid-cols-10 gap-1">
+        <img src={hubnex28} alt="" className="h-[155px] w-full"  />
+        <img src={hubnex2} alt="" className="h-[155px] w-full" />
+        <img src={hubnex3} alt="" className="h-[155px] w-full" />
+        <img src={hubnex4} alt="" className="h-[155px] w-full" />
+        <img src={hubnex5} alt="" className="h-[155px] w-full" />
+        <img src={hubnex6} alt="" className="h-[155px] w-full" />
+        <img src={hubnex7} alt="" className="h-[155px] w-full" />
+        <img src={hubnex2} alt="" className="h-[155px] w-full" />
+        <img src={hubnex4} alt="" className="h-[155px] w-full" />
+        <img src={hubnex19} alt="" className="h-[155px] w-full" />
+        {/*  */}
+        <img src={hubnex12} alt="" className="h-[155px] w-full" />
+        <img src={hubnex11} alt="" className="h-[155px] w-full" />
+        <img src={hubnex12} alt="" className="h-[155px] w-full" />
+        <img src={hubnex13} alt="" className="h-[155px] w-full" />
+        <img src={hubnex14} alt="" className="h-[155px] w-full" />
+        <img src={hubnex15} alt="" className="h-[155px] w-full" />
+        <img src={hubnex16} alt="" className="h-[155px] w-full" />
+        <img src={hubnex17} alt="" className="h-[155px] w-full" />
+        <img src={hubnex18} alt="" className="h-[155px] w-full" />
+        <img src={hubnex14} alt="" className="h-[155px] w-full" />
+        {/*  */}
+        <img src={hubnex29} alt="" className="h-[155px] w-full" />
+        <img src={hubnex19} alt="" className="h-[155px] w-full" />
+        <img src={hubnex20} alt="" className="h-[155px] w-full" />
+        <img src={hubnex21} alt="" className="h-[155px] w-full" />
+        <img src={hubnex22} alt="" className="h-[155px] w-full" />
+        <img src={hubnex23} alt="" className="h-[155px] w-full" />
+        <img src={hubnex24} alt="" className="h-[155px] w-full" />
+        <img src={hubnex25} alt="" className="h-[155px] w-full" />
+        <img src={hubnex26} alt="" className="h-[155px] w-full" />
+        <img src={hubnex32} alt="" className="h-[155px] w-full" />
+        {/*  */}
+        <img src={hubnex4} alt="" className="h-[155px] w-full" />
+        <img src={hubnex27} alt="" className="h-[155px] w-full" />
+        <img src={hubnex28} alt="" className="h-[155px] w-full" />
+        <img src={hubnex29} alt="" className="h-[155px] w-full" />
+        <img src={hubnex30} alt="" className="h-[155px] w-full" />
+        <img src={hubnex31} alt="" className="h-[155px] w-full" />
+        <img src={hubnex2} alt="" className="h-[155px] w-full" />
+        <img src={hubnex32} alt="" className="h-[155px] w-full" />
+        <img src={hubnex33} alt="" className="h-[155px] w-full" />
+        <img src={hubnex16} alt="" className="h-[155px] w-full" />
+        {/*  */}
+        <img src={hubnex25} alt="" className="h-[155px] w-full" />
+        <img src={hubnex2} alt="" className="h-[155px] w-full" />
+        <img src={hubnex3} alt="" className="h-[155px] w-full" />
+        <img src={hubnex4} alt="" className="h-[155px] w-full" />
+        <img src={hubnex5} alt="" className="h-[155px] w-full" />
+        <img src={hubnex6} alt="" className="h-[155px] w-full" />
+        <img src={hubnex7} alt="" className="h-[155px] w-full" />
+        <img src={hubnex2} alt="" className="h-[155px] w-full" />
+        <img src={hubnex4} alt="" className="h-[155px] w-full" />
+        <img src={hubnex6} alt="" className="h-[155px] w-full" />
+        {/*  */}
+        <img src={hubnex27} alt="" className="h-[155px] w-full" />
+        <img src={hubnex19} alt="" className="h-[155px] w-full" />
+        <img src={hubnex20} alt="" className="h-[155px] w-full" />
+        <img src={hubnex21} alt="" className="h-[155px] w-full" />
+        <img src={hubnex22} alt="" className="h-[155px] w-full" />
+        <img src={hubnex23} alt="" className="h-[155px] w-full" />
+        <img src={hubnex24} alt="" className="h-[155px] w-full" />
+        <img src={hubnex25} alt="" className="h-[155px] w-full" />
+        <img src={hubnex26} alt="" className="h-[155px] w-full" />
+        <img src={hubnex24} alt="" className="h-[155px] w-full" />
+        
+       
+      </div>
+
+      <div className="absolute bottom-0 left-0 right-0 bg-white/50 backdrop-blur-md h-96  pt-24">
+        <h1 className="text-4xl font-semibold text-center">
+          Unlocking Investment Opportunities with Hubnex Tech Fund
+        </h1>
+        <p className="text-center mt-2">Empowering startups with Hubnex Tech Fund</p>
+        <div className="flex justify-center mt-4">
+          <button className="px-14 py-2 bg-black text-white rounded">
+            Apply
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Scrollingcard;
