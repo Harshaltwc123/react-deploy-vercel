c1f4b8628df8ddf0c63c5e502f859a35b6aa11bc
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 17:24:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Implements from "./component/Implements";
 import Investmentopportunity from "./component/Investmentopportunity";
 import Landing from "./component/Landing";
 import Scrollingcard from "./component/Scrollingcard";
+import Unleash from "./component/Unleash";
 
 
 
@@ -28,6 +29,7 @@ function App() {
 <Investmentopportunity/>
 <Landing/>
 <Scrollingcard/>
+<Unleash/>
 
  </>
  
