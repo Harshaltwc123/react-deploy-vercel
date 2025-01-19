c391fe69a86bad08a4167a8be5281fe6a878ea61
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 16:53:07 +0530

    first commit3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Brands from "./component/Brands";
+import Ecosystemdevelopment from "./component/Ecosystemdevelopment";
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
  <>
  <Brands/>
+ <Ecosystemdevelopment/>
  </>
  
   );
