ac75c7fcbe83d5fae2481f1a258cc2e93f557190
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 17:37:26 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,32 @@
 import React from "react";
-import Brands from "./component/Brands";
-import Faq from "./component/Faq";
-import Flipcard from "./component/Flipcard";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import WhatWeOffer from "./pages/WhatWeOffer";
+import Events from "./pages/Events";
+import HubnexLabs from "./pages/HubnexLabs";
+import GetInTouch from "./pages/GetInTouch";
+import AboutUs from "./pages/AboutUs";
+import ApplyNow from "./pages/ApplyNow";
 import Footer from "./component/Footer";
-import Guide from "./component/Guide";
-import Home1 from "./component/Home1";
-import Implements from "./component/Implements";
-import Landing from "./component/Landing";
-import Scrollingcard from "./component/Scrollingcard";
-import Unleash from "./component/Unleash";
 
 
 function App() {
   return (
-    <>
-      <Brands />
-      <Faq />
-      <Flipcard />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/what-we-offer" element={<WhatWeOffer />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/hubnexlabs" element={<HubnexLabs />} />
+        <Route path="/get-in-touch" element={<GetInTouch />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/apply-now" element={<ApplyNow />} />
+      </Routes>
       <Footer />
-      <Guide />
-      <Home1 />
-      <Implements />
-      <Landing />
-      <Scrollingcard />
-      <Unleash />
-    </>
+    </Router>
+ 
   );
 }
 
