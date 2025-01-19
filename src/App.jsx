4272bc261e4b7c00d62b8e2441b7124d409c7e5d
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 17:35:23 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,29 @@
 import React from "react";
 import Brands from "./component/Brands";
-import Ecosystemdevelopment from "./component/Ecosystemdevelopment";
 import Faq from "./component/Faq";
 import Flipcard from "./component/Flipcard";
 import Footer from "./component/Footer";
 import Guide from "./component/Guide";
 import Home1 from "./component/Home1";
 import Implements from "./component/Implements";
-import Investmentopportunity from "./component/Investmentopportunity";
 import Landing from "./component/Landing";
 import Scrollingcard from "./component/Scrollingcard";
 import Unleash from "./component/Unleash";
-import Feedbackcard from "./component/Feedbackcard";
+
 
 function App() {
   return (
     <>
       <Brands />
-      <Ecosystemdevelopment />
       <Faq />
       <Flipcard />
       <Footer />
       <Guide />
       <Home1 />
       <Implements />
-      <Investmentopportunity />
       <Landing />
       <Scrollingcard />
       <Unleash />
-      <Feedbackcard/>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,23 +4,19 @@ import Home1 from "../component/Home1";
 import Brands from "../component/Brands";
 import Unleash from "../component/Unleash";
 import Implements from "../component/Implements";
-import FeedbackCard from "../component/Feedbackcard";
 import Faq from "../component/Faq";
 import Landing from "../component/Landing";
 import Flipcard from "../component/Flipcard";
-// import Scrollingcard from "../component/Scrollingcard";
 
 function Home() {
   return (
-    <div className='mt-10'>
-      <Flipcard/>
-      {/* <Scrollingcard/> */}
+    <div className="mt-10">
+      <Flipcard />
       <Brands />
       <Home1 />
       <Guide />
       <Unleash />
       <Implements />
-      <FeedbackCard />
       <Faq />
       <Landing />
     </div>
