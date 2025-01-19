7c961e46cfc9523467c144956155c0d10fe6d13d
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 16:29:54 +0530

    thirty commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
 import React from 'react'
-import Brands from './component/Brands'
-import Ecosystemdevelopment from './component/Ecosystemdevelopment'
-import Faq from './component/Faq'
-import Feedbackcard from './component/Feedbackcard'
-import Flipcard from './component/Flipcard'
-import Footer from './component/Footer'
-import Guide from './component/Guide'
-import Home1 from './component/Home1'
-import Implements from './component/Implements'
-import Investmentopportunity from './component/Investmentopportunity'
-import Landing from './component/Landing'
-import Scrollingcard from './component/Scrollingcard'
-import Strategicguidence from './component/Strategicguidence'
-import Technologyenablement from './component/Technologyenablement'
-import Unleash from './component/Unleash'
 
 function App() {
   return (
     <div>
-      <Brands/>
-      <Ecosystemdevelopment/>
-      <Faq/>
-      <Feedbackcard/>
-      <Flipcard/>
-      <Footer/>
-      <Guide/>
-      <Home1/>
-      <Implements/>
-      <Investmentopportunity/>
-      <Landing/>
-      <Scrollingcard/>
-      <Strategicguidence/>
-      <Technologyenablement/>
-      <Unleash/>
+      <h1>Hello</h1>
     </div>
   )
 }
