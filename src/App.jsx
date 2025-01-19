eca9f25a7716107175bcb83bb7768834bf190efd
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 16:46:06 +0530

    first commit1

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,17 @@ import Faq from "./component/Faq";
 import Ecosystemdevelopment from "./component/Ecosystemdevelopment";
 import Feedbackcard from "./component/Feedbackcard";
 import Flipcard from "./component/Flipcard";
+import Footer from "./component/Footer";
+import Guide from "./component/Guide";
+import Home1 from "./component/Home1";
+import Implements from "./component/Implements";
+import Investmentopportunity from "./component/Investmentopportunity";
+import Landing from "./component/Landing";
+import Scrollingcard from "./component/Scrollingcard";
+import Strategicguidence from "./component/Strategicguidence";
+import Technologyenablement from "./component/Technologyenablement";
+import Unleash from "./component/Unleash";
+import Navbar from "./component/Navbar";
 
 
 
@@ -11,11 +22,23 @@ function App() {
   return (
    
  <div>
+
   <Brands/>
   <Faq/>
   <Ecosystemdevelopment/>
   <Feedbackcard/>
   <Flipcard/>
+  <Footer/>
+  <Guide/>
+  <Home1/>
+  <Implements/>
+  <Investmentopportunity/>
+  <Landing/>
+  <Scrollingcard/>
+  <Strategicguidence/>
+  <Technologyenablement/>
+  <Unleash/>
+
  </div>
   );
 }
