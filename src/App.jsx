6f2664fb7abbf9113aeec9f040813e0c6456762a
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 16:27:43 +0530

    thirty commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
-import React from "react";
-import Brands from "./component/Brands";
-import Faq from "./component/Faq";
-import Ecosystemdevelopment from "./component/Ecosystemdevelopment";
-
-
+import React from 'react'
+import Brands from './component/Brands'
+import Ecosystemdevelopment from './component/Ecosystemdevelopment'
+import Faq from './component/Faq'
+import Feedbackcard from './component/Feedbackcard'
+import Flipcard from './component/Flipcard'
+import Footer from './component/Footer'
+import Guide from './component/Guide'
+import Home1 from './component/Home1'
+import Implements from './component/Implements'
+import Investmentopportunity from './component/Investmentopportunity'
+import Landing from './component/Landing'
+import Scrollingcard from './component/Scrollingcard'
+import Strategicguidence from './component/Strategicguidence'
+import Technologyenablement from './component/Technologyenablement'
+import Unleash from './component/Unleash'
 
 function App() {
   return (
-   
- <div>
-  <Brands/>
-  <Faq/>
-  <Ecosystemdevelopment/>
- </div>
-  );
+    <div>
+      <Brands/>
+      <Ecosystemdevelopment/>
+      <Faq/>
+      <Feedbackcard/>
+      <Flipcard/>
+      <Footer/>
+      <Guide/>
+      <Home1/>
+      <Implements/>
+      <Investmentopportunity/>
+      <Landing/>
+      <Scrollingcard/>
+      <Strategicguidence/>
+      <Technologyenablement/>
+      <Unleash/>
+    </div>
+  )
 }
 
-export default App;
+export default App
