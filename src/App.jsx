cf17fc947a85f036e5fd744b1e90e2a7034946b0
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 19:10:30 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import React from "react";
-import Slidecard from "./component/Slidecard";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import WhatWeOffer from "./pages/WhatWeOffer";
+import Events from "./pages/Events";
+import HubnexLabs from "./pages/HubnexLabs";
+import GetInTouch from "./pages/GetInTouch";
+import AboutUs from "./pages/AboutUs";
+import ApplyNow from "./pages/ApplyNow";
+import Footer from "./component/Footer";
 
 
 function App() {
   return (
-    <div>
-      <Slidecard/>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/what-we-offer" element={<WhatWeOffer />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/hubnexlabs" element={<HubnexLabs />} />
+        <Route path="/get-in-touch" element={<GetInTouch />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/apply-now" element={<ApplyNow />} />
+      </Routes>
+      <Footer />
+    </Router>
  
   );
 }
