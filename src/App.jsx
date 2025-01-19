a0f53260254a2e5b7d7f8dacb167f9bfa05243a5
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 16:38:20 +0530

    thirty four commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Brands from './component/Brands'
+import Ecosystemdevelopment from './component/Ecosystemdevelopment'
+import Faq from './component/Faq'
+import Feedbackcard from './component/Feedbackcard'
 
 function App() {
   return (
     <div>
       <Brands/>
+      <Ecosystemdevelopment/>
+      <Faq/>
+      <Feedbackcard/>
     </div>
   )
 }
