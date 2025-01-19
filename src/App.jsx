5bb0573590e6169332a548ae6df8585b696261e9
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-19 16:57:19 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-import Brands from "./component/Brands";
-import Ecosystemdevelopment from "./component/Ecosystemdevelopment";
-import Faq from "./component/Faq";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Navbar";
+import Home from "./pages/Home";
+import WhatWeOffer from "./pages/WhatWeOffer";
+import Events from "./pages/Events";
+import HubnexLabs from "./pages/HubnexLabs";
+import GetInTouch from "./pages/GetInTouch";
+import AboutUs from "./pages/AboutUs";
+import ApplyNow from "./pages/ApplyNow";
+import Footer from "./component/Footer";
 
 
 
 function App() {
   return (
  <>
- <Brands/>
- <Ecosystemdevelopment/>
- <Faq/>
+<Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/what-we-offer" element={<WhatWeOffer />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/hubnexlabs" element={<HubnexLabs />} />
+        <Route path="/get-in-touch" element={<GetInTouch />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/apply-now" element={<ApplyNow />} />
+      </Routes>
+      <Footer />
+    </Router>
  </>
  
   );
