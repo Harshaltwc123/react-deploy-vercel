c69380a2ec9e523e88a3cafc6857b5d8406ead88
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-20 12:00:44 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Strategic from "./component/Strategic";
+import Technology from "./component/Technology";
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import Navbar from "./component/Navbar";
 // import Home from "./pages/Home";
@@ -28,7 +29,7 @@ function App() {
     //   <Footer />
     // </Router>
     <div>
-      <Strategic/>
+      <Technology/>
     </div>
  
   );
