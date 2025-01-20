b92672991be5c91cab14b05658afea0e7d1c09f0
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-20 12:09:41 +0530

    first commit

--- src/component/Technology.jsx
+++ src/component/Technology.jsx
@@ -19,11 +19,11 @@ function Technology() {
         />
         <div className="absolute bottom-0 left-0 right-0 bg-white/50 backdrop-blur-md py-6 sm:py-8 md:py-10 flex flex-col justify-center items-center gap-2 text-center">
           <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold">
-            Ecosystem Development
+            Technology Enablement
           </h1>
           <p className="text-sm sm:text-base md:text-lg">
-            Create a thriving ecosystem that supports sustainable growth and
-            long-term success!
+            Empower your business with cutting-edge technology to streamline
+            operations and drive growth!
           </p>
           <button className="bg-black text-white py-2 px-8 sm:px-10 md:px-12 rounded-md mt-4 hover:bg-purple-600 hover:border hover:border-white">
             Apply
@@ -32,55 +32,48 @@ function Technology() {
       </div>
 
       <div className="flex justify-evenly flex-wrap gap-10 my-20">
-        
         <div className="flex flex-col justify-center items-center gap-4 bg-white max-w-80 rounded-md px-4 py-12 border border-gray-200 shadow-xl">
           <img src={img2} alt="" className="rounded h-24 w-28" />
-          <h1 className="text-xl font-medium">Collaborative efforts</h1>
+          <h1 className="text-xl font-medium">Technical Expertise</h1>
           <p className="text-[16px] text-center">
             Startups partnering with Hubnex Invest benefit from seasoned
             technologists and industry experts who understand cutting-edge
-            technologies and trends.
+            technologies and trends
           </p>
         </div>
 
-        
         <div className="flex flex-col justify-center text-center items-center gap-4 bg-white max-w-80 rounded-md px-4 py-12 border border-gray-200 shadow-xl">
           <img src={img3} alt="" className="rounded h-24 w-28" />
-          <h1 className="text-xl font-medium">
-            Partnerships with Accelerators & Incubators
-          </h1>
+          <h1 className="text-xl font-medium">Product Development</h1>
           <p className="text-[16px] text-center">
-            Hubnex Invest partners with accelerators and incubators to offer
-            startups all opportunities, accelerating their growth and scaling
-            their businesses.
+            Hubnex Invest offers end-to-end product development services to help
+            startups create scalable, innovative solutions from concept to
+            launch.
           </p>
         </div>
       </div>
 
       <div className="flex justify-evenly flex-wrap gap-10 my-20">
-        
         <div className="flex flex-col justify-center text-center items-center gap-4 bg-white max-w-80 rounded-md px-4 py-12 border border-gray-200 shadow-xl">
           <img src={img4} alt="" className="rounded h-24 w-28" />
           <h1 className="text-xl font-semibold">
-            Venture Capital Partnerships
+          Technical Architecture Design
           </h1>
           <p className="text-[16px] text-center">
-            Hubnex Invest partners with venture capital firms to co-invest in
-            startups, providing capital and leveraging industry expertise to
-            support their growth and success.
+          Startups often struggle with designing scalable technical
+            architectures. Hubnex Invest offers services to create secure,
+            efficient architectures that support growth.
           </p>
         </div>
 
-        
         <div className="flex flex-col justify-center text-center items-center gap-4 bg-white max-w-80 rounded-md px-4 py-12 border border-gray-200 shadow-xl">
           <img src={img5} alt="" className="rounded h-24 w-28" />
           <h1 className="text-xl font-semibold">
-            GIndustry Associations and Agencies
+          Implementation Support
           </h1>
           <p className="text-[16px] text-center">
-            Hubnex Invest collaborates with industry associations and government
-            agencies to advocate for policies supporting startup growth and
-            innovation
+          Hubnex Invest offers implementation support to help startups
+          integrate technology solutions seamlessly into their workflows.
           </p>
         </div>
       </div>
@@ -90,10 +83,10 @@ function Technology() {
 
         <div className="text-center text-white py-10 absolute top-1/2 left-1/2  transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-0 justify-center items-center">
           <h1 className="text-xl sm:text-2xl md:text-3xl xl:text-3xl font-bold px-1 md:px-0 ">
-            Join our dynamic ecosystem to accelerate your growth!
+          Empowering Innovation through Cutting-Edge Technology Solutions!
           </h1>
           <h2 className="text-sm sm:text-base md:text-lg px-4 md:px-0">
-            Fostering a Dynamic Network for Collaborative Growth at Hubnex
+          With Hubnex’s technology solutions drive your innovation!
           </h2>
 
           <ul className="flex flex-wrap justify-center gap-4 mt-4 sm:mt-6">
