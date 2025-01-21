9dd3c7394424195d44448c2d4e3c9f8001934d13
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-21 18:01:41 +0530

    second commit

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import img from "../assets/Aboutus1.png"
 
 function AboutUs() {
   return (
     <div className='mt-28'>
-      About Us Page
+      <div className='relative'>
+        <img src={img} alt="" className='w-full h-full' />
+        <div className='absolute top-[40%] w-2/4 pl-28'>
+        <h1 className='text-5xl font-medium'> Exceptional IT services, delivering innovative solutions</h1>
+        </div>
+      </div>
     </div>
   )
 }
