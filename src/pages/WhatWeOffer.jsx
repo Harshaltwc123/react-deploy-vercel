4ab956f6c054d52de935384b0ae8e7369705d56c
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-23 15:28:13 +0530

    eleventh commit

--- src/pages/WhatWeOffer.jsx
+++ src/pages/WhatWeOffer.jsx
@@ -1,35 +1,46 @@
-import React from "react";
-import img from "../assets/WhatWeOffer.png";
+// import React from "react";
+// import img from "../assets/WhatWeOffer.png";
 
+
+// function WhatWeOffer() {
+//   return (
+//     <>
+//       <div className="flex flex-col justify-center items-center relative">
+//         {/* Image with responsive adjustments */}
+//         <img
+//           src={img}
+//           className="w-full h-auto object-cover"
+//           alt="What We Offer"
+//         />
+
+//         {/* Overlay Content */}
+//         <div className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center flex flex-col justify-center items-center gap-4 px-4">
+//           {/* Responsive Heading */}
+//           <h1 className="text-1xl md:text-2xl lg:text-3xl font-bold text-white max-w-5xl">
+//             Empowering Startups from Ideation to Success
+//           </h1>
+
+//           {/* Responsive Paragraph */}
+//           <p className="text-white text-sm md:text-base lg:text-sm max-w-2xl">
+//             Hubnex Tech Fund offers expert support and tailored solutions to
+//             accelerate growth
+//           </p>
+//         </div>
+//       </div>
+      
+//     </>
+//   );
+// }
+
+// export default WhatWeOffer;
+import React from 'react'
 
 function WhatWeOffer() {
   return (
-    <>
-      <div className="flex flex-col justify-center items-center relative">
-        {/* Image with responsive adjustments */}
-        <img
-          src={img}
-          className="w-full h-auto object-cover"
-          alt="What We Offer"
-        />
-
-        {/* Overlay Content */}
-        <div className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center flex flex-col justify-center items-center gap-4 px-4">
-          {/* Responsive Heading */}
-          <h1 className="text-1xl md:text-2xl lg:text-3xl font-bold text-white max-w-5xl">
-            Empowering Startups from Ideation to Success
-          </h1>
-
-          {/* Responsive Paragraph */}
-          <p className="text-white text-sm md:text-base lg:text-sm max-w-2xl">
-            Hubnex Tech Fund offers expert support and tailored solutions to
-            accelerate growth
-          </p>
-        </div>
-      </div>
-      
-    </>
-  );
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
 }
 
-export default WhatWeOffer;
+export default WhatWeOffer
