26aa47f85ac3a517d8af3af2fddc48324bac3785
Harshaltwc123 <harshaltwc004@gmail.com>
2025-01-23 15:30:46 +0530

    eleventh commit

--- src/pages/WhatWeOffer.jsx
+++ src/pages/WhatWeOffer.jsx
@@ -1,5 +1,5 @@
 // import React from "react";
-// import img from "../assets/WhatWeOffer.png";
+import img from "../assets/WhatWeOffer.png";
 
 
 // function WhatWeOffer() {
@@ -37,9 +37,29 @@ import React from 'react'
 
 function WhatWeOffer() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+   <>
+          <div className="flex flex-col justify-center items-center relative">
+         
+        <img
+          src={img}
+          className="w-full h-auto object-cover"
+          alt="What We Offer"
+        />
+
+      
+        <div className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center flex flex-col justify-center items-center gap-4 px-4">
+          
+          <h1 className="text-1xl md:text-2xl lg:text-3xl font-bold text-white max-w-5xl">
+            Empowering Startups from Ideation to Success
+          </h1>
+
+          <p className="text-white text-sm md:text-base lg:text-sm max-w-2xl">
+            Hubnex Tech Fund offers expert support and tailored solutions to
+            accelerate growth
+          </p>
+        </div>
+      </div>
+   </>
   )
 }
 
